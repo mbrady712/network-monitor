f5b2e5fa616bd5e8208afbc0768e19c392d37bbe
Michael Brady <michaelbrady712@gmail.com>
2024-12-04 12:48:06 -0600

    Added functionality to view computer's arp table

---
+++ network-monitor/Controllers/NetworkController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using network_monitor.Helpers;
+
+namespace network_monitor.Controllers
+{
+    public class NetworkController : Controller
+    {
+        public ActionResult ArpResults()
+        {
+            // Call the utility method to get arp -a results
+            string arpResults = NetworkUtils.ExecuteArpCommand();
+
+            // Pass the results to the view
+            ViewBag.ArpResults = arpResults;
+
+            return View();
+        }
+    }
+
+}
---
+++ network-monitor/Helpers/NetworkUtils.cs
@@ -0,0 +1,41 @@
+﻿namespace network_monitor.Helpers
+{
+    using System.Diagnostics;
+
+    public static class NetworkUtils
+    {
+        public static string ExecuteArpCommand()
+        {
+            try
+            {
+                // Initialize the process start info
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c arp -a",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                // Start the process
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+
+                    // Read the standard output and wait for process to exit
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    return output;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error executing command: {ex.Message}";
+            }
+        }
+    }
+
+}
